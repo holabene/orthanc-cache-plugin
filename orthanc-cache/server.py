dbe760a5b0a204706511341ce565664fa2bbe98e
Bene <holabenedict@gmail.com>
2023-01-16 19:44:34 +0100

    Add Last-Modified header based on LastUpdate metadata

--- orthanc-cache/server.py
+++ orthanc-cache/server.py
@@ -1,17 +1,41 @@
 import orthanc
+import re
+from datetime import datetime
 
 
 def cached_response(output, uri, **request):
+    """
+    Methods available in output
+    'AnswerBuffer', 'CompressAndAnswerJpegImage', 'CompressAndAnswerPngImage', 'Redirect', 'SendHttpStatus',
+    'SendHttpStatusCode', 'SendMethodNotAllowed', 'SendMultipartItem', 'SendUnauthorized', 'SetCookie',
+    'SetHttpErrorDetails', 'SetHttpHeader', 'StartMultipartAnswer''AnswerBuffer',
+    'CompressAndAnswerJpegImage', 'CompressAndAnswerPngImage', 'Redirect',
+    'SendHttpStatus', 'SendHttpStatusCode', 'SendMethodNotAllowed', 'SendMultipartItem',
+    'SendUnauthorized', 'SetCookie', 'SetHttpErrorDetails', 'SetHttpHeader', 'StartMultipartAnswer'
+    """
     if not request['method'] in ['GET', 'HEAD']:
-        raise Exception("Method not supported by cache plugin")
+        output.SendMethodNotAllowed(request['method'])
+        return
 
-    print(f'Cached response for {uri} from the orthanc-cache-plugin')
+    orthanc.LogInfo(f'Cached response for {uri} from the orthanc-cache-plugin')
 
     # Get the response from Rest API
-    response = orthanc.RestApiGet(uri)
+    response = orthanc.RestApiGet(uri).decode('utf-8')
+
+    # Parse uri
+    path = re.match('/studies/([-a-z0-9]+)', uri)
+    uuid = path[1]
+
+    # Check last update
+    # TODO: check timezone
+    last_update = orthanc.RestApiGet(f'/studies/{uuid}/metadata/LastUpdate').decode('utf-8')
+    last_modified = datetime.strptime(last_update, '%Y%m%dT%H%M%S').strftime('%a, %d %b %Y %H:%M:%S')
+
+    # Add cache control
+    output.SetHttpHeader('Last-Modified', last_modified)
 
     # Add response to output
     output.AnswerBuffer(response, 'application/json')
 
 
-orthanc.RegisterRestCallback('/system', cached_response)
+orthanc.RegisterRestCallback('/studies/([-a-z0-9]+)', cached_response)
