0745348f51f245ed46ddfe378bf94a1ebea01ee1
Bene <holabenedict@gmail.com>
2023-01-16 19:49:32 +0100

    Add level and passthrough

--- orthanc-cache/server.py
+++ orthanc-cache/server.py
@@ -19,23 +19,21 @@ def cached_response(output, uri, **request):
 
     orthanc.LogInfo(f'Cached response for {uri} from the orthanc-cache-plugin')
 
-    # Get the response from Rest API
-    response = orthanc.RestApiGet(uri).decode('utf-8')
-
     # Parse uri
-    path = re.match('/studies/([-a-z0-9]+)', uri)
-    uuid = path[1]
+    path = re.match('/(patients|studies|series|instances)/([-a-z0-9]+)', uri)
+    level = path[1]
+    uuid = path[2]
 
     # Check last update
     # TODO: check timezone
-    last_update = orthanc.RestApiGet(f'/studies/{uuid}/metadata/LastUpdate').decode('utf-8')
+    last_update = orthanc.RestApiGet(f'/{level}/{uuid}/metadata/LastUpdate').decode('utf-8')
     last_modified = datetime.strptime(last_update, '%Y%m%dT%H%M%S').strftime('%a, %d %b %Y %H:%M:%S')
 
     # Add cache control
     output.SetHttpHeader('Last-Modified', last_modified)
 
-    # Add response to output
-    output.AnswerBuffer(response, 'application/json')
+    # Passthrough
+    output.AnswerBuffer(orthanc.RestApiGet(uri), 'application/json')
 
 
-orthanc.RegisterRestCallback('/studies/([-a-z0-9]+)', cached_response)
+orthanc.RegisterRestCallback('/(patients|studies|series|instances)/([-a-z0-9]+)', cached_response)
