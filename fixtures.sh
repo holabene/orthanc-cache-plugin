1b5a9881985eabac7a3f0f864cfd43b13a8d2d44
Bene <holabenedict@gmail.com>
2023-02-01 18:09:07 +0100

    Add import test

---
+++ fixtures.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Specify the output directory and base file name
+output_dir=.data/test
+base_file=image
+
+# Create the output directory
+mkdir -p ${output_dir}
+
+# Generate study and series instance UID
+StudyInstanceUID="1.2.3.$(date +%s)"
+SeriesInstanceUID="1.2.4.$(date +%s)"
+
+# Generate 100 dummy DICOM images
+for i in $(seq -w 1 100); do
+  output_file=${output_dir}/${base_file}${i}.dcm
+  SOPInstanceUID="1.2.5.$i"
+
+  cp test.dcm "${output_file}"
+
+  dcmodify -nb \
+  -i "InstanceNumber=$i" \
+  -i "SOPInstanceUID=$SOPInstanceUID" \
+  -i "SeriesInstanceUID=$SeriesInstanceUID" \
+  -i "StudyInstanceUID=$StudyInstanceUID" \
+  "${output_file}"
+done
---
+++ tests/ImportDicom.test.js
@@ -0,0 +1,44 @@
+import { check, sleep } from 'k6'
+import { Httpx } from 'https://jslib.k6.io/httpx/0.0.1/index.js'
+
+export let options = {
+    vus: 1,
+    iterations: 1,
+    duration: '2h',
+    setupTimeout: '30m',
+    teardownTimeout: '5m',
+}
+
+let files = []
+
+for (let i = 1; i <= 100; i++) {
+  let filePath = `../.data/test/image${i.toString().padStart(3, "0")}.dcm`;
+  files.push(open(filePath, 'b'));
+}
+
+const session = new Httpx({
+    baseURL: 'http://orthanc:orthanc@localhost:8042',
+})
+
+export function setup() {
+    // nothing yet
+}
+
+export default function (data) {
+    // Import DICOM file
+    files.forEach((file, index) => {
+        const res = session.post('/instances', file, { timeout: '15m' })
+
+        check(res, {
+            'status is 200': (r) => r.status === 200,
+        })
+
+        const data = res.json()
+        const instanceId = data['ID']
+        console.log(`Uploaded study #${index + 1} ${instanceId}`)
+    })
+}
+
+export function teardown(data) {
+    // nothing yet
+}
