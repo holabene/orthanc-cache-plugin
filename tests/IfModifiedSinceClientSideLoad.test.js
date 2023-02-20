fc5cb63cd6acc0ec5a254d7b06086860ca22f432
Bene <holabenedict@gmail.com>
2023-02-20 17:14:30 +0100

    Load test for server side and client side cache

---
+++ tests/IfModifiedSinceClientSideLoad.test.js
@@ -0,0 +1,61 @@
+import { check, sleep, fail } from 'k6'
+import { Httpx } from 'https://jslib.k6.io/httpx/0.0.1/index.js'
+
+export let options = {
+    vus: 100,
+    duration: '10s',
+}
+
+let studyIds = []
+
+const session = new Httpx({
+    baseURL: 'http://orthanc:orthanc@localhost:8042',
+})
+
+export function setup() {
+    const res = session.get('/studies')
+    studyIds = res.json()
+
+    const testData = []
+
+    // make requests to /studies/{id}/shared-tags without If-Modified-Since header
+    // to populate cache
+    studyIds.forEach((studyId, index) => {
+        const res = session.get(`/studies/${studyId}/shared-tags`)
+
+        const checkOutput = check(res, {
+            'Status is 200 or 304': (r) => [200, 304].includes(r.status),
+            'Last-Modified is not empty': (r) => r.headers['Last-Modified'] !== '',
+        })
+
+        testData.push({ studyId, lastModified: res.headers['Last-Modified'] })
+
+        // sleep for 1 second
+        sleep(1)
+    })
+
+    return { testData }
+}
+
+export default function (data) {
+    data.testData.forEach(({ studyId, lastModified }, index) => {
+        // Make call to shared-tags with If-Modified-Since header to current time
+        const res = session.get(`/studies/${studyId}/shared-tags`, null, {
+            headers: {
+                'If-Modified-Since': new Date().toUTCString(),
+            }
+        })
+
+        // check status code is 200 or 304
+        const checkOutput = check(res, {
+            'With If-Modified-Since to current time, status is 200 or 304': (r) => [200, 304].includes(r.status)
+        })
+
+        // sleep for 1 second
+        sleep(1)
+    })
+}
+
+export function teardown(data) {
+    // nothing yet
+}
