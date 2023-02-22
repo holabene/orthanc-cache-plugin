92c4b3b28a28708eb381626a570ad4dc9f3814d1
Bene <holabenedict@gmail.com>
2023-02-22 17:58:39 +0100

    Accept parameter for number of series/images for fixtures and produce json

--- tests/fixtures.sh
+++ tests/fixtures.sh
@@ -7,24 +7,43 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 output_dir="${DIR}/../.data/test"
 base_file=image
 
+# Number of series and images per series (default: 1 series with 100 images)
+num_series=${1:-1}
+num_images=${2:-100}
+
 # Create the output directory
 mkdir -p "${output_dir}"
 
-# Generate study and series instance UID
+# Generate study instance UID
 StudyInstanceUID="1.2.3.$(date +%s)"
-SeriesInstanceUID="1.2.4.$(date +%s)"
 
-# Generate 100 dummy DICOM images
-for i in $(seq -w 1 100); do
-  output_file="${output_dir}/${base_file}${i}.dcm"
-  SOPInstanceUID="1.2.5.$i"
+# Generate array of objects with DICOM file path
+files=()
 
-  cp "${DIR}/test.dcm" "${output_file}"
+for (( s=1; s<=num_series; s++ )); do
+  # Generate series instance UID
+  SeriesInstanceUID="1.2.4.$(date +%s).${s}"
 
-  dcmodify -nb \
-  -i "InstanceNumber=$i" \
-  -i "SOPInstanceUID=$SOPInstanceUID" \
-  -i "SeriesInstanceUID=$SeriesInstanceUID" \
-  -i "StudyInstanceUID=$StudyInstanceUID" \
-  "${output_file}"
+  for (( i=1; i<=num_images; i++ )); do
+    # Generate file path, SOP instance UID, and object for JSON array
+    output_file="${output_dir}/series-${s}/${base_file}${i}.dcm"
+    SOPInstanceUID="1.2.5.${s}.${i}"
+
+    # Create directory for series if it doesn't exist
+    mkdir -p "$(dirname "$output_file")"
+
+    # Copy and modify template DICOM file
+    cp "${DIR}/test.dcm" "${output_file}"
+    dcmodify -nb \
+      -i "InstanceNumber=$i" \
+      -i "SOPInstanceUID=$SOPInstanceUID" \
+      -i "SeriesInstanceUID=$SeriesInstanceUID" \
+      -i "StudyInstanceUID=$StudyInstanceUID" \
+      "${output_file}"
+
+    # Add file path and series information to JSON array
+    files+=("{\"path\":\"${output_file}\"}")
+  done
 done
+
+find "${output_dir}" -name "*.dcm" -type f | jq -R . | jq -s . > "${output_dir}/files.json"
