c0b9205aaaec5a83cbefbc7c62ab95cd7777f51a
Bene <holabenedict@gmail.com>
2023-02-22 11:35:56 +0100

    Add script dir

--- tests/fixtures.sh
+++ tests/fixtures.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Specify the output directory and base file name
-output_dir=../.data/test
+output_dir="${DIR}/../.data/test"
 base_file=image
 
 # Create the output directory
-mkdir -p ${output_dir}
+mkdir -p "${output_dir}"
 
 # Generate study and series instance UID
 StudyInstanceUID="1.2.3.$(date +%s)"
@@ -13,10 +16,10 @@ SeriesInstanceUID="1.2.4.$(date +%s)"
 
 # Generate 100 dummy DICOM images
 for i in $(seq -w 1 100); do
-  output_file=${output_dir}/${base_file}${i}.dcm
+  output_file="${output_dir}/${base_file}${i}.dcm"
   SOPInstanceUID="1.2.5.$i"
 
-  cp test.dcm "${output_file}"
+  cp "${DIR}/test.dcm" "${output_file}"
 
   dcmodify -nb \
   -i "InstanceNumber=$i" \
